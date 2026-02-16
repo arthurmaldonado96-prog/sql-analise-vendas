e55c7b8d105e9eb738f08d7f86a2b745f323720e
Arthur <arthur.maldonado96@gmail.com>
2026-02-16 17:12:26 -0300

    Create 01_criacao_tabelas.sql

---
+++ 01_criacao_tabelas.sql
@@ -0,0 +1,35 @@
+-- =============================================
+-- Projeto: Análise de Vendas
+-- Autor: Arthur
+-- Banco: Microsoft SQL Server
+-- =============================================
+-- Criando banco de dados e tabelas
+
+CREATE DATABASE PortfolioVendas;
+
+
+USE PortfolioVendas;
+
+CREATE TABLE dClientes (
+    id_cliente INT PRIMARY KEY,
+    nome VARCHAR(100),
+    cidade VARCHAR(100),
+    estado CHAR(2)
+);
+
+CREATE TABLE dProdutos (
+    id_produto INT PRIMARY KEY,
+    nome_produto VARCHAR(100),
+    categoria VARCHAR(100),
+    preco DECIMAL(10,2)
+);
+
+CREATE TABLE fVendas (
+    id_venda INT PRIMARY KEY,
+    id_cliente INT,
+    id_produto INT,
+    data_venda DATETIME,
+    quantidade INT,
+    FOREIGN KEY (id_cliente) REFERENCES dClientes(id_cliente),
+    FOREIGN KEY (id_produto) REFERENCES dProdutos(id_produto)
+);
